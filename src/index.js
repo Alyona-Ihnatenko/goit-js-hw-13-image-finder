30144823c23d2c7b172fbc57e7a9b9931e2d92bb
Alyona Ihnatenko <altaig4@gmail.com>
2021-07-11 16:02:16 +0300

    end-hw-13

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 import './sass/main.scss';
+// import './styles/style.scss';
+// import './styles/bLithBoxStyles.scss';
+import './scripts/application.js';
+import './scripts/modalLightBox.js';
+
---
+++ src/scripts/apiService.js
@@ -0,0 +1,28 @@
+import {error} from './notification.js';
+export default {
+  page:1,
+  search: '',
+  per_page: 12,
+  APIkey: '22450906-3e002c9d2b529a68e38cedb68',
+  baseURL: "https://pixabay.com/api/?image_type=photo&orientation=horizontal&q",
+  async getResourse   ()  {
+    try{
+      const responce = await fetch(`${this.baseURL}=${this.search}&page=${this.page}&per_page=${this.per_page}&key=${this.APIkey}`);
+      this.page +=1;
+
+      return await  responce.json();
+    }
+    catch(err) {
+      throw error({
+         text:(`Ошибка по ${this.search} статус${responce.status}`),
+         hide:true,
+         delay:3000,
+       });
+      
+    }
+  },
+  resetPage(){
+  this.page = 1;
+  },
+  
+}
